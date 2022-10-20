80e118915ba3f4c2c7b8b71761b4508e241ac529
Alymamy Gbané <alymamygbane25@gmail.com>
2022-10-20 06:46:34 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * file - main.h
+ * Author: Alymamy Gbane
+ * Desc: Header contains protypes for all functions
+ *      used in 0x03-more_functions_nested_loops
+ */
+int _putchar(char c);
+
+
+
+
+
+
+#endif
+       
