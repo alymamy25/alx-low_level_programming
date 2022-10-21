5d03ab11770d0b07c77dc2b296f17939059a3ef0
Alymamy Gbané <alymamygbane25@gmail.com>
2022-10-21 08:22:08 -0700

    0 Task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Test function for posite or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	postitive_or_negative(i);
+
+	return (0);
+}
+
